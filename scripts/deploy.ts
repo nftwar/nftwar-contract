bb92955223ed59d638ededdea78f49adfc90f308
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2022-10-15 13:14:13 +0000

    deploy: Add battle deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,6 @@
 import { ethers } from "hardhat";
 
-async function main() {
+async function nft() {
   const NFTWARbetaPARTSFactory = await ethers.getContractFactory("NFTWARbetaPARTS");
   const NFTWARbetaPARTS = await NFTWARbetaPARTSFactory.deploy("https://nftwar.games/api/v1/nftwarbetaparts/metadata/");
 
@@ -9,9 +9,18 @@ async function main() {
   console.log(NFTWARbetaPARTS.address);
 }
 
+async function battle(){
+  const battleFactory = await ethers.getContractFactory("NFTWARbetaBattle");
+  const battle = await battleFactory.deploy("0xbD557213066076568E003927034f5a8dD997E215");
+
+  await battle.deployed();
+
+  console.log(battle.address);
+}
+
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
+battle().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
