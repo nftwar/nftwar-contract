1d9da4ffbeb73f73fd616699558549f70a016615
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2022-10-17 07:50:26 +0000

    feat: update battle logic

--- contracts/beta/beta_battle.sol
+++ contracts/beta/beta_battle.sol
@@ -11,6 +11,7 @@ contract NFTWARbetaBattle is Ownable, ContractGuard {
         PartsContract =  parts;
     }
     
+    event Battle(uint256 userDEF, uint256 userATK, uint256 enemyDEF, uint256 enemyATK);
     event Result(bool isWin);
 
     function battle(uint256[] memory tokenIds, uint256[] memory enemeyTokenIds) external returns (bool isWin) {
@@ -33,52 +34,42 @@ contract NFTWARbetaBattle is Ownable, ContractGuard {
             enemyATK += PartsContract.getATK(enemeyTokenIds[i]);
         }
 
-        // 유저 방어력이 상대 공격보다 더 높을 시 
-        if(userDEF >= enemyATK){
-            // 유저 공격이 상대 방어보다 높으면 이김 (한대)
-            if(userATK >= enemyDEF){
-                emit Result(true);
-                return true;
-            }
-            // 유저 공격이 상대 방어보다 낮지만, 유저 공격이 더 센 경우 (여러대 선승)
-            else if(userDEF - enemyATK >= enemyDEF - userATK){
-                emit Result(true);
-                return true;
-            }
-            // 유저가 먼저 쓰러짐 (여러방 패)
-            return false;
-        }
-        else{// 유저 방어력이 상대 공격보다 낮을 시
-            // 유저 공격이 상대 방어보다 더 높음 - 서로 한대 - 유저가 이김
-            if(userATK >= enemyDEF){
-                emit Result(true);
-                return true;
-            }
-
-            // 한대맞고 죽음
-            emit Result(false);
-            return false;
-        }
+        emit Battle(userDEF, userATK, enemyDEF, enemyATK);
 
         // uint이기때문에 userDEF > enemyATK 라면 프로그램이 터짐
-        // // 선공 후공 그냥 막 잡는다
-        // if (block.timestamp % 2 == 0) {
-        //     while(userDEF > 0 && enemyDEF > 0) {
-        //         userDEF -= enemyATK;
-        //         enemyDEF -= userATK;
-        //     }
+        // 선공 후공 랜덤
+        if (block.timestamp % 2 == 0) {
+            // 후공
+            while(true) {
+                if(enemyATK >= userDEF){
+                    emit Result(false);
+                    return false;
+                }
+                userDEF -= enemyATK;
 
-        // } else {
-        //     while(userDEF > 0 && enemyDEF > 0) {
-        //         enemyDEF -= userATK;
-        //         userDEF -= enemyATK;
-        //     }
-        // }
-        // if(userDEF > 0) {
-        //     return (true);
-        // } else {
-        //     return (false);
-        // }
+                if(userATK >= enemyDEF){
+                    emit Result(true);
+                    return true;
+                }
+                enemyDEF -= userATK;
+            }
+
+        } else {
+            // 선공
+            while(true) {
+                if(userATK >= enemyDEF){
+                    emit Result(true);
+                    return true;
+                }
+                enemyDEF -= userATK;
+
+                if(enemyATK >= userDEF){
+                    emit Result(false);
+                    return false;
+                }
+                userDEF -= enemyATK;
+            }
+        }
     }
 
     function getLatestUnit(address _user) public view returns (uint256[] memory){
